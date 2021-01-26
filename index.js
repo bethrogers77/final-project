cf591cd6a38cd7900eb2b177a34fc1138a50e543
bethannrogers77@gmail.com <bethannrogers77@gmail.com>
2021-01-26 11:41:48 -0600

    updates jan 26

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ catfactCollection.on('value', function (results) {
 })
 
 // Update
-$('.catfact-board').on('click', 'a.upvote', function(event) {
+$('.catfact-board').on('click', 'a.upvote', function (event) {
   event.preventDefault();
 
   const key = $(event.target).closest('li').attr('data-catfact-id');
@@ -76,7 +76,7 @@ $('.catfact-board').on('click', 'a.upvote', function(event) {
   });
 })
 
-$('.catfact-board').on('click', 'a.downvote', function(event) {
+$('.catfact-board').on('click', 'a.downvote', function (event) {
   event.preventDefault();
 
   const key = $(event.target).closest('li').attr('data-catfact-id');
@@ -92,7 +92,7 @@ $('.catfact-board').on('click', 'a.downvote', function(event) {
 })
 
 // Delete
-$('.catfact-board').on('click', 'a.remove', function(event) {
+$('.catfact-board').on('click', 'a.remove', function (event) {
   event.preventDefault();
 
   const key = $(event.target).closest('li').attr('data-catfact-id');
@@ -100,3 +100,45 @@ $('.catfact-board').on('click', 'a.remove', function(event) {
 
   catfactObj.remove();
 });
+
+// Feedr project
+// adding cat fact API from Feedr project
+(function () {
+  'use strict';
+
+  function cat() {
+    $('#source').text('Cat Facts')
+    let $xhr = $.getJSON("https://cat-fact.herokuapp.com/facts")
+    $xhr.done(function (data) {
+      if ($xhr.status != 200) {
+        return;
+      }
+      console.log(data);
+      for (let i = 0; i < 4; i += 1) {
+        $('h5').eq(i).text(data[i].text);
+      }
+    })
+  }
+
+  function dog() {
+    $('#source').text('Dog Facts')
+    let $xhr = $.getJSON("https://cat-fact.herokuapp.com/facts/random?animal_type=dog&amount=4")
+    $xhr.done(function (data) {
+      if ($xhr.status != 200) {
+        return;
+      }
+      console.log(data);
+      for (let i = 0; i < 4; i += 1) {
+        $('h53').eq(i).text(data[i].text);
+      }
+    })
+  }
+  cat();
+  $('#cat').on('click', function () {
+    cat();
+  });
+
+  $('#dog').on('click', function () {
+    dog();
+  });
+})()
